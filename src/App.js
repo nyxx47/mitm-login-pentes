da9293a4bcc122ce87d566be447a814a6c622ffe
krisna ahroid <krisna@clappingape.com>
2020-06-15 14:27:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [data, setData] = useState(null)
+
+  const endpoint = "https://reqres.in/api/login"
+  // eve.holt@reqres.in
+  // cityslicka
+  const payload = {
+    "email": username,
+    "password": password
+}
+  const handleLogin = () => {
+    axios.post(endpoint, payload).then(res => {
+      
+      setData(res.data.token)
+    })
+  }
+  
+  return(
+    <div>
+      <input type="text" value={username} onChange={event => setUsername(event.target.value)}/>
+      <input type="password" value={password} onChange={event => setPassword(event.target.value)}/>
+      <button onClick={() => handleLogin()}>Login</button>
+      <p>Data: {data}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
